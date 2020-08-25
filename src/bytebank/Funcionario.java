7b08241d65bff4a3dfe0e1c678b03047ea38b588
Vitor Gabriel <bielvitor2008@hotmail.com>
2020-08-25 18:41:25 -0300

    Sobrescrito o metodo getBonificacao de forma correta.

--- src/bytebank/Funcionario.java
+++ src/bytebank/Funcionario.java
@@ -7,7 +7,7 @@ public class Funcionario {
 	protected double salario;
 	
 	public double getBonificacao() {
-		return this.salario * 0.1;
+		return this.salario * 0.05;
 	}
 	
 	public String getNome() {
