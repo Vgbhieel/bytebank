7455ff044f1d93d1c0136299000874ee20ef153b
Vitor Gabriel <bielvitor2008@hotmail.com>
2020-09-03 19:08:11 -0300

    Projeto ajustado de acordo com o novo módulo do curso.

---
+++ src/bytebank/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package bytebank;
+
+public class ContaPoupanca extends Conta {
+
+	ContaPoupanca(int agencia, int numero){
+		super(agencia, numero);
+	}
+	
+}
--- src/bytebank/EditorVideo.java
+++
@@ -1,9 +0,0 @@
-package bytebank;
-
-public class EditorVideo extends Funcionario {
-	
-	public double getBonificacao() {
-		return (super.getBonificacao() + 100);
-	}
-	
-} 
